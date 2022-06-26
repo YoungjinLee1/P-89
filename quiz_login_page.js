4274ec940041ef3c8255f76122ab49b06fe2ef1c
YoungjinLee1 <79812873+YoungjinLee1@users.noreply.github.com>
2022-06-26 09:26:18 -0300

    Add files via upload

--- quiz_login_page.js
+++ quiz_login_page.js
@@ -1,4 +1,7 @@
-adduser()
-{
-
+function addUser() {
+  var p1 = document.getElementById("p1-name").value;
+  var p2 = document.getElementById("p2-name").value;
+  localStorage.setItem("player1", p1);
+  localStorage.setItem("player2", p2);
+  window.location = "quiz_game_page.html";
 }
